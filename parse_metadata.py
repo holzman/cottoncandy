ff6d79a24a2a1c59fd82bf882c29f357e0a55e47
Burt Holzman <burt@fnal.gov>
2017-05-08 12:16:22 -0500

    Utility script for AWS metadata

---
+++ parse_metadata.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import json, urllib
+
+u = urllib.urlopen('http://169.254.169.254/latest/meta-data/iam/security-credentials/AllowS3_UploadDownload')
+j = json.load(u)
+print "export AWS_ACCESS_KEY=%s" % j['AccessKeyId']
+print "export AWS_SECRET_KEY=%s" % j['SecretAccessKey']
+print "export TOKEN=%s" % j['Token']
+
