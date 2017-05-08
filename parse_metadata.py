a34bc67a132de4fa39178bab404f86c8fdbca58b
Burt Holzman <burt@fnal.gov>
2017-05-08 14:15:37 -0500

    Change to proper sec group

--- parse_metadata.py
+++ parse_metadata.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import json, urllib
 
-u = urllib.urlopen('http://169.254.169.254/latest/meta-data/iam/security-credentials/AllowS3_UploadDownload')
+u = urllib.urlopen('http://169.254.169.254/latest/meta-data/iam/security-credentials/AllowS3_Download')
 j = json.load(u)
 print "export AWS_ACCESS_KEY=%s" % j['AccessKeyId']
 print "export AWS_SECRET_KEY=%s" % j['SecretAccessKey']
